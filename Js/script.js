5f8afaf2f797a467ec732f1e7f465dee30ac437e
ruba <roooby.2000@gmail.com>
2021-06-23 11:51:08 +0300

    dispalying logo input + adding new rows button in the table

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,4 @@
 function myFunction() {
-
- 
   RadioBoxCheck();
 }
 
@@ -34,11 +32,12 @@ function RadioBoxCheck() {
   }
 }
 
-function isEmpty(inputId,inputDiv,pId) {//inpui id, input error message wrapper, input text (p)
+function isEmpty(inputId, inputDiv, pId) {
+  //inpui id, input error message wrapper, input text (p)
   let compname = document.getElementById(inputId).value; //get compnum value
   let text = ""; //initiate empty string
 
-  if (compname.length==0) {
+  if (compname.length == 0) {
     //if not correct
     text = "يجب تعبئه هذا الصندوق";
     document.getElementById(inputDiv).style.visibility = "unset"; //unset div visibilty
@@ -49,3 +48,40 @@ function isEmpty(inputId,inputDiv,pId) {//inpui id, input error message wrapper,
   }
 }
 
+var loadFile = function (event) {
+  var image = document.getElementById("output");
+  image.src = URL.createObjectURL(event.target.files[0]);
+};
+
+function addRow() {
+  var table = document.getElementById("table");
+  var rows = document.getElementById("table").rows.length;
+  // add row
+  var row = table.insertRow(rows);
+
+  // add input in cell
+  for (var i = 1; i <= 4; i++) {
+    var cell1 = row.insertCell(i);
+    var inputItem = document.createElement("input");
+    cell1.appendChild(inputItem);
+  }
+  table.appendChild(tr);
+}
+
+function add() {
+  document.getElementById("table").insertRow(1).innerHTML =
+    '<tr><th scope="row"><input type="text"id="contactname" ></th> <td><input type="number" id="contactIdNum" ></td> <td><input type="number" id="contactIdNum1"></td> <td><input type="text" id="contactOption" ></td></tr>';
+}
+function add1() {
+  document.getElementById("table1").insertRow(1).innerHTML =
+    '<tr><th scope="row"><input type="text"id="contactname" ></th> <td><input type="number" id="contactIdNum" ></td> <td><input type="number" id="contactIdNum1"></td> <td><input type="text" id="contactOption" ></td></tr>';
+}
+
+/*
+<tr>
+  <th scope="row"> <input type="text"id="contactname"></th>
+  <td><input type="number" id="contactIdNum" ></td> 
+  <td><input type="number" id="contactIdNum1"></td>
+  <td><input type="text" id="contactOption" ></td>
+</tr>';
+*/
