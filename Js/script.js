71b17563b5992cc91b9bdd6b4b400d9f3a05af36
Ruba <ruba.almutairi00@gmail.com>
2021-06-29 14:41:39 +0300

    did local storage things for the two tables in input.html

--- Js/script.js
+++ Js/script.js
@@ -69,7 +69,6 @@ function addRow() {
   
 }
 
-var infos=[];
 
 
 function addrow() {
@@ -78,8 +77,11 @@ function addrow() {
  
 }
 
+
+
+var contacts=[];
 var row=1;
-function addinput(){
+function addcontact(){
   let contactname=document.getElementById("contactname").value;
   let contactIdNum=document.getElementById("contactIdNum").value;
   let contactIdNum1=document.getElementById("contactIdNum1").value;
@@ -89,7 +91,18 @@ function addinput(){
     alert("fill input");
     return;
   }
-  
+
+  var entry = {
+    "contactname": contactname,
+    "contactIdNum": contactIdNum,
+    "contactIdNum1":contactIdNum1,
+    "contactOption":contactOption
+
+};
+localStorage.setItem("entry", JSON.stringify(entry));
+contacts.push(entry);
+
+
   let table =document.getElementById("table");
   let newrow=table.insertRow(row);
   let cell1=newrow.insertCell(0);
@@ -128,7 +141,50 @@ function add1() {
   <td><input type="text" id="contactOption"></td>
 </tr>';
 */
+var stores=[];
+var row1=1;
+function addstore(){
+  let Storename=document.getElementById("Storename").value;
+  let city=document.getElementById("city").value;
+  let distric=document.getElementById("distric").value;
+  let storeOption=document.getElementById("storeOption").value;
 
+  if(!(Storename||city||distric||storeOption)){
+    alert("fill input");
+    return;
+  }
+
+  var entry = {
+    "Storename": Storename,
+    "city": city,
+    "distric":distric,
+    "storeOption":storeOption
+
+};
+localStorage.setItem("entry", JSON.stringify(entry));
+contacts.push(entry);
+
+
+  let table =document.getElementById("table1");
+  let newrow=table.insertRow(row1);
+  let cell1=newrow.insertCell(0);
+  let cell2=newrow.insertCell(1);
+  let cell3=newrow.insertCell(2);
+  let cell4=newrow.insertCell(3);
+
+cell1.innerHTML=Storename;
+cell2.innerHTML=city;
+cell3.innerHTML=city;
+cell4.innerHTML=storeOption;
+row++;
+
+document.getElementById("Storename").value="";
+  document.getElementById("city").value="";
+document.getElementById("distric").value="";
+  document.getElementById("storeOption").value="";
+
+
+}
 function addCard(){
   document.getElementById("cardtemp").insertAdjacentHTML("afterend",'<div class="card card-1"> <div class="colm-1"> <div class="img-card"> <img class="company-logo" data-src="https://www.jarir.com/skin/frontend/jarir/default/images/svg/logo.svg" alt="logo" src="https://www.jarir.com/skin/frontend/jarir/default/images/svg/logo.svg" style="margin: 10%;"> </div> <div class="info company-name"> <h4 style="color:#0E8A86;">متاجر مكتبة جرير</h4> <h5>شركه جرير المحدوده</h5> </div> <div class="vl1 last-vl"></div> <div class="info more-info"> <h4 style="color:#353C43;">رقم السجل</h4> <h5>10345876732</h5> </div> <div class="vl1"></div> <div class="info more-info"> <h4>عدد المتاجر</h4> <h5>5 متاجر</h5> </div> <div class="vl1"></div> <div class="info more-info"> <h4>رصيد التخفيضات</h4> <h5>30 يوم</h5> </div> <div class="vl1 last-vl"></div> <div class="info more-info last-btn" onclick="location.href="#"" style="cursor: pointer;"> <h3>استعراض المتجر</h3> <i class="bi bi-chevron-left"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" /> </svg></i> </div> </div> </div>')
 }
